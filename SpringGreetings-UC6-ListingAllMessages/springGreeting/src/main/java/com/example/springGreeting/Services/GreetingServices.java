d8a2fb81823c78934aadb204f381aeb3be776ac1
arun <arunupadhyay7836@gmail.com>
2025-03-03 23:34:53 +0530

    Added files of UC-6 Listing All Messages

---
+++ SpringGreetings-UC6-ListingAllMessages/springGreeting/src/main/java/com/example/springGreeting/Services/GreetingServices.java
@@ -0,0 +1,56 @@
+package com.example.springGreeting.Services;
+
+import com.example.springGreeting.Entities.MessageEntity;
+import com.example.springGreeting.Repository.GreetingRepository;
+import com.example.springGreeting.model.Greeting;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Service
+public class GreetingServices {
+    @Autowired
+    String greeting;
+    GreetingRepository greetingRepository;
+
+    public GreetingServices() {
+        greeting = "Hello User";
+        this.greetingRepository = greetingRepository;
+
+    }
+    public String getGreeting() {
+        return this.greeting;
+    }
+    public void setGreeting(String greeting) {
+        this.greeting = greeting;
+    }
+    public Greeting save(Greeting message){
+        MessageEntity me = new MessageEntity(message.getMessage());
+
+        greetingRepository.save(me);
+
+        Greeting Info = new Greeting(me.getMessage());
+
+        Info.setMessageID(me.getId());
+
+        return Info;
+    }
+    public Greeting findbyID(Long ID){
+        MessageEntity me = greetingRepository.findById(ID).orElseThrow(()->new RuntimeException("No Record Found"));
+        Greeting Info = new Greeting(me.getMessage());
+        Info.setMessageID(me.getId());
+        return Info;
+    }
+    public List<Greeting> getAll(){
+        List<Greeting> list  = greetingRepository.findAll().stream().map(me ->{
+            Greeting Info = new Greeting(me.getMessage());
+            Info.setMessageID(me.getId());
+            return Info;
+        }).collect(Collectors.toList());
+        return list;
+    }
+}
---
+++ SpringGreetings-UC6-ListingAllMessages/springGreeting/src/main/java/com/example/springGreeting/collector/GreetingController.java
@@ -0,0 +1,70 @@
+package com.example.springGreeting.collector;
+import com.example.springGreeting.Services.GreetingServices;
+import com.example.springGreeting.model.Greeting;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import jakarta.websocket.server.PathParam;
+import java.util.List;
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+
+    GreetingServices greetingServices;
+    public GreetingController(GreetingServices greetingServices) {
+        this.greetingServices = greetingServices;
+    }
+    Greeting message;
+    @GetMapping("/get")
+    public String greeting() {
+        return "Hello World";
+    }
+    @PostMapping("/post")
+    public String greetingPost(@RequestBody Greeting message) {
+        return "Hello Post Request from"+message.getMessage();
+    }
+    @PutMapping("/put/{message}")
+    public String greetingPut(@PathVariable String message) {
+        return "Hello Put Request from"+message;
+    }
+    @DeleteMapping("/delete/{message}")
+    public String greetingDelete(@PathVariable String message) {
+        return "Hello Delete Request from"+message;
+    }
+    @PatchMapping("/patch/{message}")
+    public String greetingPatch(@PathVariable String message) {
+        return "Hello Patch Request from"+message;
+    }
+
+    @GetMapping("/services")
+    public String greetingServices() {
+        return greetingServices.getGreeting();
+    }
+
+    @GetMapping("/query")
+    public String query(@PathParam("firstName") String firstName, @PathParam("lastName") String lastName){
+        if(firstName != null && lastName != null)
+            return "Hello "+firstName+" "+lastName+"Welcome to My Application";
+        else if(firstName != null)
+            return "Hello "+firstName+" Welcome to Application";
+        else if(lastName != null)
+            return "Hello "+lastName+" Welcome to Application";
+        else
+            return "Hello, Welcome to Application";
+    }
+
+    @PostMapping("/save")
+    public String save(@RequestBody Greeting message){
+        return greetingServices.save(message).getMessage();
+    }
+    @GetMapping("/find/{ID}")
+    public Greeting findbyID(@PathVariable Long ID){
+        return greetingServices.findbyID(ID);
+    }
+    @GetMapping("/all")
+    public List<Greeting> getAll(){
+        return greetingServices.getAll();
+
+    }
+
+}
