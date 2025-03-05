47b18ff65e111b07e51542c0542345715bc79881
arun <arunupadhyay7836@gmail.com>
2025-03-05 23:35:54 +0530

    Added new feature of sending mail to successfully registered Users

---
+++ SpringGreetings-UC11-SendingMails/springGreeting/src/main/java/com/example/springGreeting/collector/UserController.java
@@ -0,0 +1,43 @@
+package com.example.springGreeting.collector;
+
+import com.example.springGreeting.DTO.LoginDTO;
+import com.example.springGreeting.DTO.MailDTO;
+import com.example.springGreeting.DTO.MessageDTO;
+import com.example.springGreeting.DTO.AuthUserDTO;
+import com.example.springGreeting.Services.AuthenticationService;
+import com.example.springGreeting.Services.EmailService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    EmailService emailService;
+    AuthenticationService authenticationService;
+
+    public UserController(EmailService emailService, AuthenticationService authenticationService) {
+        this.emailService = emailService;
+        this.authenticationService = authenticationService;
+    }
+
+    //UC9 --> For Registration of a user
+    @PostMapping(path = "/register")
+    public String register(@RequestBody AuthUserDTO user) {
+        return authenticationService.register(user);
+    }
+
+
+    //UC10 --> For User Login
+    @PostMapping(path ="/login")
+    public String login(@RequestBody LoginDTO user){
+        return authenticationService.login(user);
+    }
+
+    //UC11 --> For sending mail to another person
+    @PostMapping(path = "/sendMail")
+    public String sendMail(@RequestBody MailDTO message){
+        emailService.sendEmail(message.getTo(), message.getSubject(), message.getBody());
+        return "Mail sent";
+    }
+
+}
